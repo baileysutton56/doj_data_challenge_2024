4225a77fda044281c76498ffd115a8a356c7a895
Bailey Sutton <56436303+baileysutton56@users.noreply.github.com>
2025-04-19 13:08:56 -0400

    Update file paths

--- data-cleaning/acs_data_cleaning/acs-pop-sex-clean.R
+++ data-cleaning/acs_data_cleaning/acs-pop-sex-clean.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(readxl)
 
 # Read 2020 data
-pop_2020 <- read_excel('H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/2020/population-by-sex-2020.xlsx', sheet='Sheet1')
+pop_2020 <- read_excel('/3. Import Data/Census ACS/2020/population-by-sex-2020.xlsx', sheet='Sheet1')
 
 # Remove commas from values
 pop_2020 <- pop_2020 %>%
@@ -18,8 +18,8 @@ pop_2020 <- pop_2020 %>%
 
 
 
-Population <- read.csv('H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/Population.csv')
-Sex <- read.csv('H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/Sex Total.csv')
+Population <- read.csv('/3. Import Data/Census ACS/Population.csv')
+Sex <- read.csv('/3. Import Data/Census ACS/Sex Total.csv')
 
 # Select relevant columns
 Population <- Population %>%
@@ -48,7 +48,7 @@ pop_sex_clean <- inner_join(wide_pop_clean,wide_sex_clean)
 pop_sex_clean_all <- rbind(pop_sex_clean, pop_2020)
 
 # Export CSV
-write.csv(pop_sex_clean_all, 'H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/Final-Cleaned/population_sex.csv')
+write.csv(pop_sex_clean_all, '/3. Import Data/Census ACS/Final-Cleaned/population_sex.csv')
 
 
 
