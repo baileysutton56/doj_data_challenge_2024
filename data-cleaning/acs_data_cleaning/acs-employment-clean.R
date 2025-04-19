5608015b4468a82d15270df98927bdc0403e3121
Bailey Sutton <56436303+baileysutton56@users.noreply.github.com>
2025-04-19 13:06:54 -0400

    Update file paths

--- data-cleaning/acs_data_cleaning/acs-employment-clean.R
+++ data-cleaning/acs_data_cleaning/acs-employment-clean.R
@@ -12,7 +12,7 @@ library(readxl)
 
 
 # Read file with all years except 2020
-Employment <- read.csv('H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/Employment.csv')
+Employment <- read.csv('/3. Import Data/Census ACS/Employment.csv')
 
 # Select relevant columns
 Employment <- Employment %>%
@@ -44,7 +44,7 @@ wide_employment_clean <- select(wide_employment,
                                 not_labor_force)
 
 # Read 2020 data
-employment_2020 <- read_excel('H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/2020/employment-2020.xlsx', sheet='Sheet1')
+employment_2020 <- read_excel('/3. Import Data/Census ACS/2020/employment-2020.xlsx', sheet='Sheet1')
 
 # Remove commas from values
 employment_2020 <- employment_2020 %>%
@@ -58,4 +58,4 @@ employment_clean_all <- employment_clean_all %>%
   mutate_at(c(2:8), as.numeric)
 
 # Export CSV
-write.csv(employment_clean_all, 'H:/BPHC/OSBO/Data Lab/Enterprise Analytics Team/Projects/10. DOJ Data Challenge/3. Import Data/Census ACS/Final-Cleaned/employment.csv')
+write.csv(employment_clean_all, '/3. Import Data/Census ACS/Final-Cleaned/employment.csv')
